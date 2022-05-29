d7311b6efb44539f67775e0847c9ca0ab60ed1c4
Koukyosyumei <koukyosyumei@hotmail.com>
2022-05-29 11:24:53 +0900

    add a test for quantiles

--- test.sh
+++ test.sh
@@ -2,3 +2,5 @@ g++ -o test.out test_secureboost.cpp
 ./test.out < data/test_data.in
 g++ -o test.out test_metric.cpp
 ./test.out
+g++ -o test.out test_utils.cpp
+./test.out
---
+++ test_utils.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+#include <cassert>
+#include <iostream>
+#include "secureboost/utils.h"
+
+int main()
+{
+    vector<double> in = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+    vector<double> quartiles = Quantile<double>(in, {0.25, 0.5, 0.75});
+    vector<double> test_quartiles = {3.25, 6, 8.75};
+    for (int i = 0; i < quartiles.size(); i++)
+    {
+        assert(quartiles[i] == test_quartiles[i]);
+    }
+    cout << "test_utils: all passed!" << endl;
+}
