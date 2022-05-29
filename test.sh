a8052cd36b76abf3591f4c423d67ab9d9485a499
Koukyosyumei <koukyosyumei@hotmail.com>
2022-05-29 11:39:47 +0900

    change the struc of dir

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 g++ -o test.out test/test_secureboost.cpp
-./test.out < data/test_data.in
+./test.out < test/data/test_data.in
 g++ -o test.out test/test_metric.cpp
 ./test.out
 g++ -o test.out test/test_utils.cpp
