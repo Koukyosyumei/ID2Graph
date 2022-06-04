92e033d2243ac6834d631069e33944174de7e1fb
Koukyosyumei <koukyosyumei@hotmail.com>
2022-06-05 00:31:03 +0900

    vis network

--- script/network_analysis.py
+++ script/network_analysis.py
@@ -1,6 +1,8 @@
 import numpy as np
 import argparse
 import os
+import networkx as nx
+from matplotlib import pyplot as plt
 
 
 def add_args(parser):
@@ -33,3 +35,9 @@ if __name__ == "__main__":
                 temp_adj_mat.append(temp_row)
                 line_idx += 1
             list_adj_mat.append(np.array(temp_adj_mat).astype(int))
+
+    G = nx.from_numpy_matrix(sum(list_adj_mat[1:5]))
+    nx.draw_networkx(
+        G, with_labels=False, alpha=0.3, node_size=60, linewidths=0.1, width=0.1
+    )
+    plt.savefig(os.path.join(parsed_args.path_to_dir, "adj_mat_plot.png"))
