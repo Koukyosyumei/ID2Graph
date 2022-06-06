03d489bd6f3944383936f67ec4319c1a3c44665b
Koukyosyumei <koukyosyumei@hotmail.com>
2022-06-06 22:12:33 +0900

    add test_louvain

--- test.sh
+++ test.sh
@@ -2,5 +2,7 @@ g++ -o test.out test/test_secureboost.cpp
 ./test.out < test/data/test_data.in
 g++ -o test.out test/test_metric.cpp
 ./test.out
+g++ -o test.out test/test_louvain.cpp
+./test.out
 g++ -o test.out test/test_utils.cpp
 ./test.out
