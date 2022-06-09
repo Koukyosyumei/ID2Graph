112ef2ecbd751c33e63df57d785ef1c7090720ff
Koukyosyumei <koukyosyumei@hotmail.com>
2022-06-10 00:18:16 +0900

    update

--- script/run.sh
+++ script/run.sh
@@ -1,13 +1,15 @@
-while getopts d: OPT
+while getopts d:r:c: OPT
 do
   case $OPT in
     "d" ) FLG_D="TRUE" ; VALUE_D="$OPTARG" ;;
+    "r" ) FLG_R="TRUE" ; VALUE_R="$OPTARG" ;;
+    "c" ) FLG_C="TRUE" ; VALUE_C="$OPTARG" ;;
   esac
 done
 
 TEMPD=$(mktemp -d -t ci-$(date +%Y-%m-%d-%H-%M-%S)-XXXXXXXXXX --tmpdir=result)
 
-script/run_training.sh -d ${VALUE_D} -p ${TEMPD} -r 5 -c 1 > "${TEMPD}/result.ans"
+script/run_training.sh -d ${VALUE_D} -p ${TEMPD} -r ${VALUE_R} -c ${VALUE_C} > "${TEMPD}/result.ans"
 script/run_extract_result.sh -o ${TEMPD}
 python3 script/clustering.py -p ${TEMPD}
 # python3 script/network_analysis.py -p ${TEMPD}
