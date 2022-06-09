bb4039f3f03303e518aeb77e5939ee33a08ef37b
Koukyosyumei <koukyosyumei@hotmail.com>
2022-06-09 17:49:45 +0900

    update

--- script/run_training.sh
+++ script/run_training.sh
@@ -8,14 +8,14 @@ do
   esac
 done
 
-g++ -o pipeline.out pipeline.cpp
-g++ -o pipeline_louvain.out pipeline_louvain.cpp
+g++ -o script/pipeline.out script/build/pipeline.cpp
+g++ -o script/pipeline_louvain.out script/build/pipeline_louvain.cpp
 
 for i in $(seq 1 5)
 do 
     echo "random seed is $i"
     python3 ./data/prep.py -d ${VALUE_D} -p "./data/${VALUE_D}/" -s $i
     cp "data/${VALUE_D}/${VALUE_D}.in" "${VALUE_P}/${VALUE_D}_${i}.in"
-    ./pipeline.out -f ${VALUE_P} -p ${i} -r ${VALUE_R} -c ${VALUE_C} < "data/${VALUE_D}/${VALUE_D}.in"
-    ./pipeline_louvain.out < "${VALUE_P}/${i}_adj_mat.txt" > "${VALUE_P}/${i}_communities.out"
+    script/build/pipeline.out -f ${VALUE_P} -p ${i} -r ${VALUE_R} -c ${VALUE_C} < "data/${VALUE_D}/${VALUE_D}.in"
+    script/build/pipeline_louvain.out < "${VALUE_P}/${i}_adj_mat.txt" > "${VALUE_P}/${i}_communities.out"
 done
