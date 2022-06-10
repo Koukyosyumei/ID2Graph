2ef482dc694a1d55820d4854e31d1fb57b098ad6
Koukyosyumei <koukyosyumei@hotmail.com>
2022-06-10 18:16:09 +0900

    add eta and completely_secure_rounds as optional arguments of pipeline_2

--- script/run.sh
+++ script/run.sh
@@ -1,10 +1,11 @@
-while getopts d:r:c:i:wn OPT
+while getopts d:r:c:i:e:wn OPT
 do
   case $OPT in
     "d" ) FLG_D="TRUE" ; VALUE_D="$OPTARG" ;;
     "r" ) FLG_R="TRUE" ; VALUE_R="$OPTARG" ;;
     "c" ) FLG_C="TRUE" ; VALUE_C="$OPTARG" ;;
     "i" ) FLG_I="TRUE" ; VALUE_I="$OPTARG" ;;
+    "e" ) FLG_E="TRUE" ; VALUE_E="$OPTARG" ;;
     "w" ) FLG_W="TRUE" ; VALUE_W="$OPTARG" ;;
     "n" ) FLG_N="TRUE" ; VALUE_N="$OPTARG" ;;
   esac
@@ -12,12 +13,12 @@ done
 
 TEMPD=$(mktemp -d -t ci-$(date +%Y-%m-%d-%H-%M-%S)-XXXXXXXXXX --tmpdir=result)
 
-echo -e "d,${VALUE_D}\nr,${VALUE_R}\nc,${VALUE_C}\ni,${VALUE_I}\nw,${FLG_W}\nn,${FLG_N}" > "${TEMPD}/param.csv"
+echo -e "d,${VALUE_D}\nr,${VALUE_R}\nc,${VALUE_C}\ni,${VALUE_I}\ne,${VALUE_E}\nw,${FLG_W}\nn,${FLG_N}" > "${TEMPD}/param.csv"
 
 if [ "${FLG_W}" = "TRUE" ]; then
-  script/run_training.sh -d ${VALUE_D} -p ${TEMPD} -r ${VALUE_R} -c ${VALUE_C} -i ${VALUE_I} -w > "${TEMPD}/result.ans"
+  script/run_training.sh -d ${VALUE_D} -p ${TEMPD} -r ${VALUE_R} -c ${VALUE_C} -i ${VALUE_I} -e ${VALUE_E}$ -w > "${TEMPD}/result.ans"
 else
-  script/run_training.sh -d ${VALUE_D} -p ${TEMPD} -r ${VALUE_R} -c ${VALUE_C} -i ${VALUE_I} > "${TEMPD}/result.ans"
+  script/run_training.sh -d ${VALUE_D} -p ${TEMPD} -r ${VALUE_R} -c ${VALUE_C} -i ${VALUE_I} -e ${VALUE_E}$ > "${TEMPD}/result.ans"
 fi
 
 script/run_extract_result.sh -o ${TEMPD}
