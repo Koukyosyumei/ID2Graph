6ebdcd6320199a349ef1bb9aa1a59aa0c2bd5fea
Koukyosyumei <koukyosyumei@hotmail.com>
2022-06-20 00:09:58 +0900

    add a tet

--- test/test_randomforest.cpp
+++ test/test_randomforest.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 const int min_leaf = 1;
-const int depth = 3;
+const int depth = 2;
 const int num_trees = 1;
 const double subsample_cols = 1.0;
 
@@ -50,13 +50,6 @@ int main()
     // --- Check Training --- //
     clf.fit(parties, y);
 
-    vector<double> predict_raw = clf.predict_raw(X);
-    for (int i = 0; i < predict_raw.size(); i++)
-    {
-        cout << predict_raw[i] << " ";
-    }
-    cout << endl;
-
     assert(clf.estimators[0].dtree.giniimp == 0.46875);
     assert(clf.estimators[0].dtree.score == 0.16875);
     assert(clf.estimators[0].dtree.best_party_id == 0);
@@ -64,70 +57,66 @@ int main()
     assert(clf.estimators[0].dtree.best_threshold_id == 2);
 
     assert(clf.estimators[0].dtree.party_id == 0);
-    assert(get<0>(clf.estimators[0].dtree.parties->at(clf.estimators[0].dtree.party_id).lookup_table.at(clf.estimators[0].dtree.record_id)) == 0);
-    assert(get<1>(clf.estimators[0].dtree.parties->at(clf.estimators[0].dtree.party_id).lookup_table.at(clf.estimators[0].dtree.record_id)) == 16);
-
-    /*
-    assert(parties[0].get_lookup_table().size() == 4);
-    assert(parties[1].get_lookup_table().size() == 2);
-
-    assert(clf.estimators[0].dtree.get_num_parties() == 2);
-
-    vector<int> test_idxs_root = {0, 1, 2, 3, 4, 5, 6, 7};
-    vector<int> idxs_root = clf.estimators[0].dtree.idxs;
-    for (int i = 0; i < idxs_root.size(); i++)
-        assert(idxs_root[i] == test_idxs_root[i]);
-    assert(clf.estimators[0].dtree.depth == 3);
-    assert(get<0>(clf.estimators[0].dtree.parties->at(clf.estimators[0].dtree.party_id).lookup_table.at(clf.estimators[0].dtree.record_id)) == 0);
-    assert(get<1>(clf.estimators[0].dtree.parties->at(clf.estimators[0].dtree.party_id).lookup_table.at(clf.estimators[0].dtree.record_id)) == 16);
-    assert(clf.estimators[0].dtree.is_leaf() == 0);
+    assert(get<0>(clf.estimators[0].dtree.parties->at(
+                                                     clf.estimators[0].dtree.party_id)
+                      .lookup_table.at(clf.estimators[0].dtree.record_id)) == 0);
+    assert(get<1>(clf.estimators[0].dtree.parties->at(
+                                                     clf.estimators[0].dtree.party_id)
+                      .lookup_table.at(clf.estimators[0].dtree.record_id)) == 16);
 
     vector<int> test_idxs_left = {0, 2, 7};
-    vector<int> idxs_left = clf.estimators[0].dtree.left->idxs;
-    for (int i = 0; i < idxs_left.size(); i++)
-        assert(idxs_left[i] == test_idxs_left[i]);
-    assert(clf.estimators[0].dtree.left->is_pure());
-    assert(clf.estimators[0].dtree.left->is_leaf());
-    assert(clf.estimators[0].dtree.left->val == 0.5074890528001861);
-
     vector<int> test_idxs_right = {1, 3, 4, 5, 6};
+    vector<int> idxs_left = clf.estimators[0].dtree.left->idxs;
+    assert(idxs_left.size() == test_idxs_left.size());
+    for (int i = 0; i < idxs_left.size(); i++)
+    {
+        assert(idxs_left[i] == test_idxs_left[i]);
+    }
     vector<int> idxs_right = clf.estimators[0].dtree.right->idxs;
+    assert(idxs_right.size() == test_idxs_right.size());
     for (int i = 0; i < idxs_right.size(); i++)
+    {
         assert(idxs_right[i] == test_idxs_right[i]);
-    assert(!clf.estimators[0].dtree.right->is_pure());
-    assert(!clf.estimators[0].dtree.right->is_leaf());
-    assert(clf.estimators[0].dtree.right->val == -0.8347166357912786);
-    XGBoostNode right_node = *clf.estimators[0].dtree.right;
-    assert(right_node.party_id == 1);
-    assert(get<0>(right_node.parties->at(right_node.party_id)
-                      .lookup_table.at(right_node.record_id)) == 0);
+    }
 
-    XGBoostNode right_right_node = *right_node.right;
-    assert(right_right_node.party_id == 0);
-    assert(get<0>(right_right_node.parties->at(right_right_node.party_id)
-                      .lookup_table.at(right_right_node.record_id)) == 0);
-    assert(get<1>(right_right_node.parties->at(right_right_node.party_id)
-                      .lookup_table.at(right_right_node.record_id)) == 25);
+    assert(clf.estimators[0].dtree.right->depth == 1);
+    assert(clf.estimators[0].dtree.left->is_leaf() == 1);
+    assert(clf.estimators[0].dtree.right->is_leaf() == 0);
 
-    assert(clf.estimators[0].dtree.right->right->left->is_leaf());
-    assert(clf.estimators[0].dtree.right->right->right->is_leaf());
-    assert(clf.estimators[0].dtree.right->right->left->val == 0.3860706492904221);
-    assert(clf.estimators[0].dtree.right->right->right->val == -0.6109404045885225);
+    assert(clf.estimators[0].dtree.right->party_id == 1);
+    assert(get<1>(clf.estimators[0].dtree.right->parties->at(
+                                                            clf.estimators[0].dtree.right->party_id)
+                      .lookup_table.at(clf.estimators[0].dtree.right->record_id)) == 0);
 
+    vector<int> test_idxs_right_left = {3, 6};
+    vector<int> test_idxs_right_right = {1, 4, 5};
+    vector<int> idxs_right_left = clf.estimators[0].dtree.right->left->idxs;
+    vector<int> idxs_right_right = clf.estimators[0].dtree.right->right->idxs;
+    for (int i = 0; i < test_idxs_right_left.size(); i++)
+    {
+        assert(test_idxs_right_left[i] == idxs_right_left[i]);
+    }
+    for (int i = 0; i < test_idxs_right_right.size(); i++)
+    {
+        assert(test_idxs_right_right[i] == idxs_right_right[i]);
+    }
+
+    vector<double> test_predict_raw = {1, 2.0 / 3.0, 1, 0, 2.0 / 3.0, 2.0 / 3.0, 0, 1};
     vector<double> predict_raw = clf.predict_raw(X);
-    vector<double> test_predcit_raw = {1.38379341, 0.53207456, 1.38379341,
-                                       0.22896408, 1.29495549, 1.29495549,
-                                       0.22896408, 1.38379341};
-    for (int i = 0; i < test_predcit_raw.size(); i++)
-        assert((predict_raw[i] - test_predcit_raw[i]) < 1e-6);
+    for (int i = 0; i < predict_raw.size(); i++)
+    {
+        assert(test_predict_raw[i] == predict_raw[i]);
+    }
 
+    vector<double> test_predict_proba = {0.7310585786300049, 0.6607563732194243,
+                                         0.7310585786300049, 0.5,
+                                         0.6607563732194243, 0.6607563732194243,
+                                         0.5, 0.7310585786300049};
     vector<double> predict_proba = clf.predict_proba(X);
-    vector<double> test_predcit_proba = {0.79959955, 0.62996684, 0.79959955,
-                                         0.55699226, 0.78498478, 0.78498478,
-                                         0.55699226, 0.79959955};
-    for (int i = 0; i < test_predcit_proba.size(); i++)
-        assert(abs(predict_proba[i] - test_predcit_proba[i]) < 1e-6);
-    */
+    for (int i = 0; i < predict_proba.size(); i++)
+    {
+        assert(abs(test_predict_proba[i] - predict_proba[i]) < 1e-6);
+    }
 
     cout << "test_randomforest: all passed!" << endl;
 }
