78120f01b3e751fbae58424889d5263f695a96b4
Koukyosyumei <koukyosyumei@hotmail.com>
2022-06-19 16:49:46 +0900

    implement a tree for rf

---
+++ src/llatvfl/randomforest/tree.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <vector>
+#include <iterator>
+#include <limits>
+#include <iostream>
+#include "../core/tree.h"
+#include "node.h"
+#include "party.h"
+
+struct RandomForestTree : Tree<RandomForestNode>
+{
+    RandomForestTree() {}
+    void fit(vector<RandomForestParty> *parties, vector<double> y,
+             int min_leaf, int depth, int active_party_id = -1, bool use_only_active_party = false, int n_job = 1)
+    {
+        vector<int> idxs(y.size());
+        iota(idxs.begin(), idxs.end(), 0);
+        for (int i = 0; i < parties->size(); i++)
+        {
+            parties->at(i).subsample_columns();
+        }
+        dtree = RandomForestNode(parties, y, idxs, depth, active_party_id, use_only_active_party, n_job);
+    }
+};
