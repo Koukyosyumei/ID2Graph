66cac9eaa5945da3afc80d6daa3fc297498009f5
Koukyosyumei <koukyosyumei@hotmail.com>
2022-06-20 18:13:31 +0900

    init implementatino of DOK sparse matrix

---
+++ src/llatvfl/utils/dok.h
@@ -0,0 +1,51 @@
+#pragma once
+#include <unordered_map>
+using namespace std;
+
+template <typename DataType>
+struct SparseMatrixDOK
+{
+    size_t dim_row = 0;
+    size_t dim_column = 0;
+    DataType zeto_val = 0;
+    unordered_map<pair<unsigned int, unsigned int>, DataType> um_ij2w;
+
+    SparseMatrixDOK(){};
+    SparseMatrixDOK(size_t dim_row_, size_t dim_column_, DataType zero_val_ = 0)
+    {
+        dim_row = dim_row_;
+        dim_column = dim_column_;
+        zero_val = zero_val_;
+    }
+
+    void add(unsigned int i, unsigned int j, DataType w)
+    {
+        pair<unsigned int, unsigned int> temp_pair = make_pair(i, j);
+        if (um_ij2w.find(temp_pair) != um_ij2w.end())
+        {
+            um_ij2w[temp_pair] = um_ij2w[temp_pair] + w;
+            if (um_ij2w[temp_pair] == zero_val)
+            {
+                um_ij2w.erase(temp_pair);
+            }
+        }
+        else
+        {
+            um_ij2w.emplace(temp_pair, w);
+        }
+    }
+
+    vector<vector<DataType>> to_adjacency_matrix(DataType init_val = 0)
+    {
+        vector<vector<DataType>> adj_mat(dim_row, vector<DataType>(dim_column, init_val));
+        unordered_map<pair<unsigned int, unsigned int>, DataType>::iterator it = um_ij2w.begin();
+
+        while (it != um_ij2w.end())
+        {
+            adj_mat[it->first.first][it->first.second] = it->second;
+            it++;
+        }
+
+        return adj_mat;
+    }
+};
