e184d7092260cbf40630c23f1f08dda9aea15bd6
Koukyosyumei <koukyosyumei@hotmail.com>
2022-06-20 21:19:56 +0900

    rename the name of the compiled files

--- script/test.sh
+++ script/test.sh
@@ -1,12 +1,14 @@
-g++ -o script/build/test.out test/test_utils.cpp
-./script/build/test.out
-g++ -o script/build/test.out test/test_metric.cpp
-./script/build/test.out
-g++ -o script/build/test.out test/test_dok.cpp
-./script/build/test.out
-g++ -pthread -o script/build/test.out test/test_randomforest.cpp
-./script/build/test.out < test/data/test_data.in
-g++ -pthread -o script/build/test.out test/test_xgboost.cpp
-./script/build/test.out < test/data/test_data.in
-g++ -o script/build/test.out test/test_louvain.cpp
-./script/build/test.out
+g++ -o script/build/test_utils.out test/test_utils.cpp
+./script/build/test_utils.out
+g++ -o script/build/test_metric.out test/test_metric.cpp
+./script/build/test_metric.out
+g++ -o script/build/test_dok.out test/test_dok.cpp
+./script/build/test_dok.out
+g++ -pthread -o script/build/test_randomforest.out test/test_randomforest.cpp
+./script/build/test_randomforest.out < test/data/test_data.in
+g++ -pthread -o script/build/test_xgboost.out test/test_xgboost.cpp
+./script/build/test_xgboost.out < test/data/test_data.in
+g++ -o script/build/test_louvain.out test/test_louvain.cpp
+./script/build/test_louvain.out
+
+rm script/build/test*
