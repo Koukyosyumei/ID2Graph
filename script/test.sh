7d5131f6cd1b05a812a4443d020d33bcbd6209b9
Koukyosyumei <koukyosyumei@hotmail.com>
2022-07-02 23:59:50 +0900

    update

--- script/test.sh
+++ script/test.sh
@@ -1,18 +1,13 @@
-g++ -O3 -mtune=native -march=native -o script/build/test_utils.out test/test_utils.cpp
-./script/build/test_utils.out
-g++ -O3 -mtune=native -march=native -o script/build/test_metric.out test/test_metric.cpp
-./script/build/test_metric.out
-g++ -O3 -mtune=native -march=native -o script/build/test_dok.out test/test_dok.cpp
-./script/build/test_dok.out
-g++ -O3 -mtune=native -march=native -pthread -o script/build/test_randomforest.out test/test_randomforest.cpp
-./script/build/test_randomforest.out <test/data/test_data.in
-g++ -O3 -mtune=native -march=native -pthread -o script/build/test_xgboost.out test/test_xgboost.cpp
-./script/build/test_xgboost.out <test/data/test_data.in
-g++ -O3 -mtune=native -march=native -o script/build/test_louvain.out test/test_louvain.cpp
-./script/build/test_louvain.out
-g++ -O3 -mtune=native -march=native -o script/build/test_lpmst.out test/test_lpmst.cpp
-./script/build/test_lpmst.out
+# clean up
+rm -rf build/*
 
+# build
+./script/build.sh
+
+# test for c++
+cd build
+ctest -V
+cd ..
+
+# test for python
 python3 -m pytest test/.
-
-rm script/build/test*
