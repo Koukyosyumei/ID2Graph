2f91b3c727205c140adfb3ce965234457298efcd
Koukyosyumei <koukyosyumei@hotmail.com>
2022-07-04 01:20:26 +0900

    update

--- src/llatvfl/utils/prime.h
+++ src/llatvfl/utils/prime.h
@@ -57,13 +57,107 @@ bool miller_rabin_primality_test(long long n, mt19937 &mt, long long k = 40)
 
     long long nm1 = n - 1;
     uniform_int_distribution<long long> distr(1, n - 1);
-    long long a;
-    for (long long i = 0; i < k; i++)
+
+    if (n < 2047)
     {
-        a = distr(mt);
-        if (cond_of_miller_rabin(d, a, n))
+        for (long long a : {2})
         {
-            return false;
+            if (cond_of_miller_rabin(d, a, n))
+            {
+                return false;
+            }
+        }
+    }
+    else if (n < 1373653)
+    {
+        for (long long a : {2, 3})
+        {
+            if (cond_of_miller_rabin(d, a, n))
+            {
+                return false;
+            }
+        }
+    }
+    else if (n < 9080191)
+    {
+        for (long long a : {31, 73})
+        {
+            if (cond_of_miller_rabin(d, a, n))
+            {
+                return false;
+            }
+        }
+    }
+    else if (n < 25326001)
+    {
+        for (long long a : {2, 3, 5})
+        {
+            if (cond_of_miller_rabin(d, a, n))
+            {
+                return false;
+            }
+        }
+    }
+    else if (n < 3215031751)
+    {
+        for (long long a : {2, 3, 5, 7})
+        {
+            if (cond_of_miller_rabin(d, a, n))
+            {
+                return false;
+            }
+        }
+    }
+    else if (n < 4759123141)
+    {
+        for (long long a : {2, 7, 61})
+        {
+            if (cond_of_miller_rabin(d, a, n))
+            {
+                return false;
+            }
+        }
+    }
+    else if (n < 2152302898747)
+    {
+        for (long long a : {2, 3, 5, 7, 11})
+        {
+            if (cond_of_miller_rabin(d, a, n))
+            {
+                return false;
+            }
+        }
+    }
+    else if (n < 3474749660383)
+    {
+        for (long long a : {2, 3, 5, 7, 11, 13})
+        {
+            if (cond_of_miller_rabin(d, a, n))
+            {
+                return false;
+            }
+        }
+    }
+    else if (n < 341550071728321)
+    {
+        for (long long a : {2, 3, 5, 7, 11, 13, 17})
+        {
+            if (cond_of_miller_rabin(d, a, n))
+            {
+                return false;
+            }
+        }
+    }
+    else
+    {
+        long long a;
+        for (long long i = 0; i < k; i++)
+        {
+            a = distr(mt);
+            if (cond_of_miller_rabin(d, a, n))
+            {
+                return false;
+            }
         }
     }
     return true;
