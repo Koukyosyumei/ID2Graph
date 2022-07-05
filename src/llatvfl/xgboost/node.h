5b6b973cc9acba7a74bdecad942c3f5152d7bcf1
Koukyosyumei <koukyosyumei@hotmail.com>
2022-07-05 20:22:54 +0900

    update

--- src/llatvfl/xgboost/node.h
+++ src/llatvfl/xgboost/node.h
@@ -17,9 +17,10 @@
 #include "party.h"
 #include "../core/node.h"
 #include "../utils/utils.h"
+#include "../paillier/paillier.h"
 using namespace std;
 
-struct XGBoostNode : Node
+struct XGBoostNode : Node<XGBoostParty>
 {
     vector<XGBoostParty> *parties;
     vector<float> gradient, hessian;
@@ -28,8 +29,8 @@ struct XGBoostNode : Node
     XGBoostNode *left, *right;
 
     XGBoostNode() {}
-    XGBoostNode(vector<XGBoostParty> *parties_, vector<float> y_, vector<float> gradient_,
-                vector<float> hessian_, vector<int> idxs_,
+    XGBoostNode(vector<XGBoostParty> *parties_, vector<float> &y_, vector<float> &gradient_,
+                vector<float> &hessian_, vector<int> &idxs_,
                 float min_child_weight_, float lam_, float gamma_, float eps_,
                 int depth_, int active_party_id_ = -1, bool use_only_active_party_ = false, int n_job_ = 1)
     {
@@ -274,24 +275,112 @@ struct XGBoostNode : Node
         }
         return true;
     }
+};
 
-    vector<float> predict(vector<vector<float>> &x_new)
+struct SecureBoostNode : XGBoostNode
+{
+    vector<SecureBoostParty> *parties;
+    vector<PaillierCipherText> gradient, hessian;
+    vector<float> vanila_gradient, vanila_hessian;
+    float min_child_weight, lam, gamma, eps;
+    bool use_only_active_party;
+    SecureBoostNode *left, *right;
+
+    SecureBoostNode() {}
+    SecureBoostNode(vector<SecureBoostParty> *parties_, vector<float> &y_,
+                    vector<PaillierCipherText> &gradient_,
+                    vector<PaillierCipherText> &hessian_,
+                    vector<float> &vanila_gradient_,
+                    vector<float> &vanila_hessian_,
+                    vector<int> &idxs_, float min_child_weight_, float lam_,
+                    float gamma_, float eps_, int depth_, int active_party_id_ = -1,
+                    bool use_only_active_party_ = false, int n_job_ = 1)
     {
-        int x_new_size = x_new.size();
-        vector<float> y_pred(x_new_size);
-        for (int i = 0; i < x_new_size; i++)
+        parties = parties_;
+        y = y_;
+        gradient = gradient_;
+        hessian = hessian_;
+        vanila_gradient = vanila_gradient_;
+        vanila_hessian = vanila_hessian_;
+        idxs = idxs_;
+        min_child_weight = min_child_weight_;
+        lam = lam_;
+        gamma = gamma_;
+        eps = eps_;
+        depth = depth_;
+        active_party_id = active_party_id_;
+        use_only_active_party = use_only_active_party_;
+        n_job = n_job_;
+
+        try
         {
-            y_pred[i] = predict_row(x_new[i]);
+            if (use_only_active_party && active_party_id > parties->size())
+            {
+                throw invalid_argument("invalid active_party_id");
+            }
         }
-        return y_pred;
+        catch (std::exception &e)
+        {
+            std::cout << e.what() << std::endl;
+        }
+
+        row_count = idxs.size();
+        num_parties = parties->size();
+
+        val = compute_weight();
+        tuple<int, int, int> best_split = find_split();
+
+        if (is_leaf())
+        {
+            is_leaf_flag = 1;
+        }
+        else
+        {
+            is_leaf_flag = 0;
+        }
+
+        if (is_leaf_flag == 0)
+        {
+            party_id = get<0>(best_split);
+            record_id = parties->at(party_id).insert_lookup_table(get<1>(best_split), get<2>(best_split));
+            make_children_nodes(get<0>(best_split), get<1>(best_split), get<2>(best_split));
+        }
+    }
+
+    void make_children_nodes(int best_party_id, int best_col_id, int best_threshold_id)
+    {
+        // TODO: remove idx with nan values from right_idxs;
+        vector<int> left_idxs = parties->at(best_party_id).split_rows(idxs, best_col_id, best_threshold_id);
+        vector<int> right_idxs;
+        for (int i = 0; i < row_count; i++)
+            if (!any_of(left_idxs.begin(), left_idxs.end(), [&](float x)
+                        { return x == idxs[i]; }))
+                right_idxs.push_back(idxs[i]);
+
+        left = new SecureBoostNode(parties, y, gradient, hessian,
+                                   vanila_gradient, vanila_hessian,
+                                   left_idxs, min_child_weight,
+                                   lam, gamma, eps, depth - 1, active_party_id, use_only_active_party);
+        if (left->is_leaf_flag == 1)
+        {
+            left->party_id = party_id;
+        }
+        right = new SecureBoostNode(parties, y, gradient, hessian,
+                                    vanila_gradient, vanila_hessian,
+                                    right_idxs, min_child_weight,
+                                    lam, gamma, eps, depth - 1, active_party_id, use_only_active_party);
+        if (right->is_leaf_flag == 1)
+        {
+            right->party_id = party_id;
+        }
     }
 
     float predict_row(vector<float> &xi)
     {
-        queue<XGBoostNode *> que;
+        queue<SecureBoostNode *> que;
         que.push(this);
 
-        XGBoostNode *temp_node;
+        SecureBoostNode *temp_node;
         while (!que.empty())
         {
             temp_node = que.front();
