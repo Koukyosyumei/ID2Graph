d032b227f902dcb810a06af70153ae3ad4ef20d1
Koukyosyumei <koukyosyumei@hotmail.com>
2022-07-16 23:02:06 +0900

    add docstrings

--- src/llatvfl/core/node.h
+++ src/llatvfl/core/node.h
@@ -9,6 +9,11 @@
 #include <tuple>
 using namespace std;
 
+/**
+ * @brief Bast structure for a node.
+ *
+ * @tparam PartyType Type of party.
+ */
 template <typename PartyType>
 struct Node
 {
@@ -33,16 +38,84 @@ struct Node
 
     Node(){};
 
+    /**
+     * @brief Get the idxs object
+     *
+     * @return vector<int>
+     */
     virtual vector<int> get_idxs() = 0;
+
+    /**
+     * @brief Get the party id object
+     *
+     * @return int
+     */
     virtual int get_party_id() = 0;
+
+    /**
+     * @brief Get the record id object
+     *
+     * @return int
+     */
     virtual int get_record_id() = 0;
+
+    /**
+     * @brief Get the value assigned to this node.
+     *
+     * @return float
+     */
     virtual float get_val() = 0;
+
+    /**
+     * @brief Get the evaluation score of this node.
+     *
+     * @return float
+     */
     virtual float get_score() = 0;
+
+    /**
+     * @brief Get the num of parties used for this node.
+     *
+     * @return int
+     */
     virtual int get_num_parties() = 0;
+
+    /**
+     * @brief Compute the weight (val) of this node.
+     *
+     * @return float
+     */
     virtual float compute_weight() = 0;
+
+    /**
+     * @brief Find the best split which gives the best score (gain).
+     *
+     * @return tuple<int, int, int>
+     */
     virtual tuple<int, int, int> find_split() = 0;
+
+    /**
+     * @brief Generate the children nodes.
+     *
+     * @param best_party_id The index of the best party.
+     * @param best_col_id The index of the best feature.
+     * @param best_threshold_id The index of the best threshold.
+     */
     virtual void make_children_nodes(int best_party_id, int best_col_id, int best_threshold_id) = 0;
+
+    /**
+     * @brief Return true if this node is a leaf.
+     *
+     * @return true
+     * @return false
+     */
     virtual bool is_leaf() = 0;
+
+    /**
+     * @brief Return true if the node is pure; the assigned labels to this node consist of a unique label.
+     *
+     * @return true
+     * @return false
+     */
     virtual bool is_pure() = 0;
-    // virtual vector<float> predict(vector<vector<float>> &x_new) = 0;
 };
