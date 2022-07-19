8c39b4c8c88960ba27d84ed62a4359ef528de639
Koukyosyumei <koukyosyumei@hotmail.com>
2022-07-19 23:51:49 +0900

    add test cases

--- test/mpi/test_mpirandomforest.cpp
+++ test/mpi/test_mpirandomforest.cpp
@@ -33,15 +33,15 @@ TEST(MPIRandomForest, MPIRandomForestClassifierTest)
     int num_party = 2;
 
     vector<float> y = {1, 0, 1, 0, 1, 1, 0, 1};
-    vector<vector<float> > X = {{12, 1},
-                                {32, 1},
-                                {15, 0},
-                                {24, 0},
-                                {20, 1},
-                                {25, 1},
-                                {17, 0},
-                                {16, 1}};
-    vector<vector<int> > feature_idxs = {{0}, {1}};
+    vector<vector<float>> X = {{12, 1},
+                               {32, 1},
+                               {15, 0},
+                               {24, 0},
+                               {20, 1},
+                               {25, 1},
+                               {17, 0},
+                               {16, 1}};
+    vector<vector<int>> feature_idxs = {{0}, {1}};
 
     boost::mpi::environment env(true);
     boost::mpi::communicator world;
@@ -53,7 +53,7 @@ TEST(MPIRandomForest, MPIRandomForestClassifierTest)
         if (i == my_rank)
         {
             int num_col = feature_idxs[i].size();
-            vector<vector<float> > x(num_row, vector<float>(num_col));
+            vector<vector<float>> x(num_row, vector<float>(num_col));
             for (int j = 0; j < num_col; j++)
             {
                 for (int k = 0; k < num_row; k++)
@@ -102,6 +102,49 @@ TEST(MPIRandomForest, MPIRandomForestClassifierTest)
 
     if (my_rank == 0)
     {
+        ASSERT_NEAR(clf.estimators[0].dtree.giniimp, 0.46875, 1e-6);
+        ASSERT_NEAR(clf.estimators[0].dtree.score, 0.16875, 1e-6);
+        ASSERT_EQ(clf.estimators[0].dtree.best_party_id, 0);
+        ASSERT_EQ(clf.estimators[0].dtree.best_col_id, 0);
+        ASSERT_EQ(clf.estimators[0].dtree.best_threshold_id, 2);
+
+        vector<int> test_idxs_left = {0, 2, 7};
+        vector<int> test_idxs_right = {1, 3, 4, 5, 6};
+        vector<int> idxs_left = clf.estimators[0].dtree.left->idxs;
+        sort(idxs_left.begin(), idxs_left.end());
+        ASSERT_EQ(idxs_left.size(), test_idxs_left.size());
+        for (int i = 0; i < idxs_left.size(); i++)
+        {
+            ASSERT_EQ(idxs_left[i], test_idxs_left[i]);
+        }
+
+        vector<int> idxs_right = clf.estimators[0].dtree.right->idxs;
+        sort(idxs_right.begin(), idxs_right.end());
+        ASSERT_EQ(idxs_right.size(), test_idxs_right.size());
+        for (int i = 0; i < idxs_right.size(); i++)
+        {
+            ASSERT_EQ(idxs_right[i], test_idxs_right[i]);
+        }
+
+        ASSERT_EQ(clf.estimators[0].dtree.right->depth, 1);
+        ASSERT_EQ(clf.estimators[0].dtree.left->is_leaf(), 1);
+        ASSERT_EQ(clf.estimators[0].dtree.right->is_leaf(), 0);
+
+        vector<int> test_idxs_right_left = {3, 6};
+        vector<int> test_idxs_right_right = {1, 4, 5};
+        vector<int> idxs_right_left = clf.estimators[0].dtree.right->left->idxs;
+        vector<int> idxs_right_right = clf.estimators[0].dtree.right->right->idxs;
+        sort(idxs_right_left.begin(), idxs_right_left.end());
+        sort(idxs_right_right.begin(), idxs_right_right.end());
+        for (int i = 0; i < test_idxs_right_left.size(); i++)
+        {
+            ASSERT_EQ(test_idxs_right_left[i], idxs_right_left[i]);
+        }
+        for (int i = 0; i < test_idxs_right_right.size(); i++)
+        {
+            ASSERT_EQ(test_idxs_right_right[i], idxs_right_right[i]);
+        }
+
         vector<float> test_predict_raw = {1, 2.0 / 3.0, 1, 0, 2.0 / 3.0, 2.0 / 3.0, 0, 1};
         for (int i = 0; i < predict_raw.size(); i++)
         {
