ca7b9fb35bccf276da70ab665fde2e5bd422a495
Koukyosyumei <koukyosyumei@hotmail.com>
2022-08-23 08:42:47 +0900

    update

--- src/llatvfl/core/tree.h
+++ src/llatvfl/core/tree.h
@@ -14,16 +14,33 @@ struct Tree
 
     Tree() {}
 
+    /**
+     * @brief Get the root node object
+     *
+     * @return NodeType&
+     */
     NodeType &get_root_node()
     {
         return *dtree;
     }
 
+    /**
+     * @brief Return the predicted value of the give new sample X
+     *
+     * @param X the new sample to be predicted
+     * @return vector<vector<float>>
+     */
     vector<vector<float>> predict(vector<vector<float>> &X)
     {
         return nodeapi.predict(&dtree, X);
     }
 
+    /**
+     * @brief Recursively extract the vector of predictions of the training data from the specified node
+     *
+     * @param node target node
+     * @return vector<pair<vector<int>, vector<vector<float>>>>
+     */
     vector<pair<vector<int>, vector<vector<float>>>> extract_train_prediction_from_node(NodeType &node)
     {
         if (node.is_leaf())
@@ -45,6 +62,11 @@ struct Tree
         }
     }
 
+    /**
+     * @brief Recursively extract the vector of predictions of the training data
+     *
+     * @return vector<vector<float>>
+     */
     vector<vector<float>> get_train_prediction()
     {
         vector<pair<vector<int>, vector<vector<float>>>> result = extract_train_prediction_from_node(dtree);
@@ -60,11 +82,24 @@ struct Tree
         return y_train_pred;
     }
 
+    /**
+     * @brief Printout the structure of this tree
+     *
+     * @param show_purity Show leaf purity of each leaf node, if true
+     * @param binary_color Color the leaf purity (red: >= 0.8, yellow: 0.8 ~ 0.7, green: 0.7 >)
+     * @param target_party_id The id of the active party
+     * @return string
+     */
     string print(bool show_purity = false, bool binary_color = true, int target_party_id = -1)
     {
         return nodeapi.print(&dtree, show_purity, binary_color, target_party_id);
     }
 
+    /**
+     * @brief Get the average of leaf purity
+     *
+     * @return float
+     */
     float get_leaf_purity()
     {
         return nodeapi.get_leaf_purity(&dtree, dtree.idxs.size());
