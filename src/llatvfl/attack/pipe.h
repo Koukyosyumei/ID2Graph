705b506a747e4a525d64926d29b3ff69aeba4840
Koukyosyumei <koukyosyumei@hotmail.com>
2022-08-27 18:13:19 +0900

    update

--- src/llatvfl/attack/pipe.h
+++ src/llatvfl/attack/pipe.h
@@ -125,7 +125,8 @@ struct QuickAttackPipeline
         run_louvain();
         concatenate_basex_with_one_hot_encoding_of_communities_allocation(base_X);
         vector<int> estimated_clusters = run_kmeans(base_X);
-        cout << "Finish Attack " << endl;
+        cout << "Finish Attack ";
+        cout << estimated_clusters.size() << endl;
         return estimated_clusters;
     }
 };
