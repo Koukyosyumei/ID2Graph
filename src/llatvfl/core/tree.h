b6877b38554ab43820788d1ef18432aef08e5749
Koukyosyumei <koukyosyumei@hotmail.com>
2022-10-15 11:34:33 +0900

    update

--- src/llatvfl/core/tree.h
+++ src/llatvfl/core/tree.h
@@ -87,6 +87,49 @@ struct Tree
         return nodeapi.to_json(&dtree);
     }
 
+    string to_html()
+    {
+        string html_literal_1 = "<!-- Styles -->\n<style>\n    "
+                                "body {\n        font-family: -apple-system, BlinkMacSystemFont, "
+                                "'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', "
+                                "'Segoe UI Emoji', 'Segoe UI Symbol';\n    }\n\n    "
+                                "#chartdiv {\n        width: 100%;\n        height: 400px;\n    }\n"
+                                "</style>\n\n<!-- Resources -->\n<script src='https://cdn.amcharts.com/lib/5/index.js'></script>\n"
+                                "<script src='https://cdn.amcharts.com/lib/5/xy.js'></script>\n"
+                                "<script src='https://cdn.amcharts.com/lib/5/hierarchy.js'></script>\n"
+                                "<script src='https://cdn.amcharts.com/lib/5/themes/Animated.js'></script>\n"
+                                "<script src='https://cdn.amcharts.com/lib/5/plugins/exporting.js'></script>\n\n"
+                                "<!-- Chart code -->\n<script>\n    am5.ready(function () "
+                                "{\n\n\n        /**\n         * ---------------------------------------\n         "
+                                "* This demo was created using amCharts 5.\n         *\n         "
+                                "* For more information visit:\n         * https://www.amcharts.com/\n         *\n         "
+                                "* Documentation is available at:\n         * https://www.amcharts.com/docs/v5/\n         "
+                                "* ---------------------------------------\n         */\n\n        // Create root and chart\n        "
+                                "var root = am5.Root.new('chartdiv');\n\n        "
+                                "root.setThemes([\n            am5themes_Animated.new(root)\n        ]);\n\n        "
+                                "var data = [";
+        string html_literal_2 = "];\n\n        "
+                                "var container = root.container.children.push(\n            "
+                                "am5.Container.new(root, {\n                "
+                                "width: am5.percent(100),\n                "
+                                "height: am5.percent(100),\n                "
+                                "layout: root.verticalLayout\n            })\n        );\n\n        "
+                                "var series = container.children.push(\n            "
+                                "am5hierarchy.Tree.new(root, {\n                "
+                                "singleBranchOnly: false,\n                downDepth: 1,\n                "
+                                "initialDepth: 5,\n                topDepth: 0,\n                "
+                                "valueField: 'value',\n                categoryField: 'name',\n                "
+                                "childDataField: 'children',\n                "
+                                "orientation: 'vertical'\n            })\n        );\n\n        "
+                                "series.data.setAll(data);\n        "
+                                "series.set('selectedDataItem', series.dataItems[0]);\n\n\n        "
+                                "var exporting = am5plugins_exporting.Exporting.new(root, {\n            "
+                                "menu: am5plugins_exporting.ExportingMenu.new(root, {}),\n        });\n    });"
+                                "\n</script>\n\n<!-- HTML -->\n<div id='chartdiv'></div>";
+
+        return html_literal_1 + to_json() + html_literal_2;
+    }
+
     /**
      * @brief Print out the structure of this tree
      *
