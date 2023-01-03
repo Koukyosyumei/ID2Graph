df3235ed8f5b907c5151b0b4ce22abecd8f7df9d
Koukyosyumei <koukyosyumei@hotmail.com>
2023-01-03 20:43:37 +0900

    update

--- data/prep.py
+++ data/prep.py
@@ -11,7 +11,8 @@ from sklearn.inspection import permutation_importance
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 
-from llatvfl.clustering import calculate_permutation_importance_for_kmeans_clustering
+from llatvfl.clustering import \
+    calculate_permutation_importance_for_kmeans_clustering
 
 
 def add_args(parser):
@@ -774,12 +775,22 @@ if __name__ == "__main__":
     if parsed_args.feature_importance == -1:
         pass
     else:
-        clf = RandomForestClassifier(random_state=parsed_args.seed)
-        clf.fit(X_train, y_train)
-        result = permutation_importance(
-            clf, X_train, y_train, n_repeats=10, random_state=parsed_args.seed
+        # clf = RandomForestClassifier(random_state=parsed_args.seed)
+        # clf.fit(X_train, y_train)
+        # result = permutation_importance(
+        #    clf, X_train, y_train, n_repeats=10, random_state=parsed_args.seed
+        # )
+        # fti = result.importances_mean  # clf.feature_importances_
+        fti = calculate_permutation_importance_for_kmeans_clustering(
+            X_train, y_train, n_classes=len(np.unique(y_train)), n_repeat=30, random_state=42
         )
-        fti = result.importances_mean  # clf.feature_importances_
+        np.save(
+            os.path.join(
+                parsed_args.path_to_dir,
+                f"{parsed_args.dataset_type}_{parsed_args.seed}_fti",
+            ),
+            fti,
+        )
 
         if parsed_args.feature_importance == 1:
             fti_idx = np.argsort(fti).tolist()
