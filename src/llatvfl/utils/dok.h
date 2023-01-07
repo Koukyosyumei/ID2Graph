e7166a8f0714d91186e3b8d49b45821684804ae3
Koukyosyumei <koukyosyumei@hotmail.com>
2023-01-08 00:47:05 +0900

    double the sparcity

--- src/llatvfl/utils/dok.h
+++ src/llatvfl/utils/dok.h
@@ -13,12 +13,18 @@ using namespace std;
  * @brief Implementation of Szudzik paring.
  *
  */
-struct HashPairSzudzik {
-  template <class T1, class T2> size_t operator()(const pair<T1, T2> &p) const {
+struct HashPairSzudzik
+{
+  template <class T1, class T2>
+  size_t operator()(const pair<T1, T2> &p) const
+  {
     size_t seed;
-    if (p.first >= p.second) {
+    if (p.first >= p.second)
+    {
       seed = p.first * p.first + p.first + p.second;
-    } else {
+    }
+    else
+    {
       seed = p.second * p.second + p.first;
     }
     return seed;
@@ -30,7 +36,9 @@ struct HashPairSzudzik {
  *
  * @tparam DataType
  */
-template <typename DataType> struct SparseMatrixDOK {
+template <typename DataType>
+struct SparseMatrixDOK
+{
   size_t dim_row = 0;
   size_t dim_column = 0;
   DataType zero_val = 0;
@@ -45,22 +53,28 @@ template <typename DataType> struct SparseMatrixDOK {
   SparseMatrixDOK(){};
   SparseMatrixDOK(size_t dim_row_, size_t dim_column_, DataType zero_val_ = 0,
                   bool is_symmetric_ = false,
-                  bool save_row2nonzero_idx_ = false) {
+                  bool save_row2nonzero_idx_ = false)
+  {
     dim_row = dim_row_;
     dim_column = dim_column_;
     zero_val = zero_val_;
     is_symmetric = is_symmetric_;
     save_row2nonzero_idx = save_row2nonzero_idx_;
 
-    if (save_row2nonzero_idx) {
+    if (save_row2nonzero_idx)
+    {
       row2nonzero_idx.resize(dim_row);
     }
   }
 
-  DataType &operator()(unsigned int i, unsigned int j) {
-    if (is_symmetric && (i < j)) {
+  DataType &operator()(unsigned int i, unsigned int j)
+  {
+    if (is_symmetric && (i < j))
+    {
       return um_ij2w[make_pair(j, i)];
-    } else {
+    }
+    else
+    {
       return um_ij2w[make_pair(i, j)];
     }
   }
@@ -72,28 +86,37 @@ template <typename DataType> struct SparseMatrixDOK {
    * @param j column index
    * @param w added value
    */
-  void add(unsigned int i, unsigned int j, DataType w) {
-    if (is_symmetric && (i < j)) {
+  void add(unsigned int i, unsigned int j, DataType w)
+  {
+    if (is_symmetric && (i < j))
+    {
       // save only the lower triangle matrix
       swap(i, j);
     }
 
     pair<unsigned int, unsigned int> temp_pair = make_pair(i, j);
-    if (um_ij2w.find(temp_pair) != um_ij2w.end()) {
+    if (um_ij2w.find(temp_pair) != um_ij2w.end())
+    {
       um_ij2w[temp_pair] = um_ij2w[temp_pair] + w;
-      if (um_ij2w[temp_pair] == zero_val) {
+      if (um_ij2w[temp_pair] == zero_val)
+      {
         um_ij2w.erase(temp_pair);
 
-        if (save_row2nonzero_idx) {
+        if (save_row2nonzero_idx)
+        {
           row2nonzero_idx[i].erase(remove_if(row2nonzero_idx[i].begin(),
                                              row2nonzero_idx[i].end(),
-                                             [j](int v) { return v == j; }),
+                                             [j](int v)
+                                             { return v == j; }),
                                    row2nonzero_idx[i].cend());
         }
       }
-    } else {
+    }
+    else
+    {
       um_ij2w.emplace(temp_pair, w);
-      if (save_row2nonzero_idx) {
+      if (save_row2nonzero_idx)
+      {
         row2nonzero_idx[i].push_back(j);
       }
     }
@@ -104,14 +127,19 @@ template <typename DataType> struct SparseMatrixDOK {
    *
    * @param densematrix
    */
-  void from_densematrix(vector<vector<DataType>> &densematrix) {
-    for (int i = 0; i < dim_row; i++) {
+  void from_densematrix(vector<vector<DataType>> &densematrix)
+  {
+    for (int i = 0; i < dim_row; i++)
+    {
       int end_pos = dim_column;
-      if (is_symmetric) {
+      if (is_symmetric)
+      {
         end_pos = i;
       }
-      for (int j = 0; j < end_pos; j++) {
-        if (densematrix[i][j] != zero_val) {
+      for (int j = 0; j < end_pos; j++)
+      {
+        if (densematrix[i][j] != zero_val)
+        {
           add(i, j, densematrix[i][j]);
         }
       }
@@ -124,14 +152,17 @@ template <typename DataType> struct SparseMatrixDOK {
    * @param init_val
    * @return vector<vector<DataType>>
    */
-  vector<vector<DataType>> to_densematrix(DataType init_val = 0) {
+  vector<vector<DataType>> to_densematrix(DataType init_val = 0)
+  {
     vector<vector<DataType>> adj_mat(dim_row,
                                      vector<DataType>(dim_column, init_val));
     auto it = um_ij2w.begin();
 
-    while (it != um_ij2w.end()) {
+    while (it != um_ij2w.end())
+    {
       adj_mat[it->first.first][it->first.second] = it->second;
-      if (is_symmetric) {
+      if (is_symmetric)
+      {
         adj_mat[it->first.second][it->first.first] = it->second;
       }
       it++;
@@ -140,15 +171,17 @@ template <typename DataType> struct SparseMatrixDOK {
     return adj_mat;
   }
 
-  float get_nonzero_ratio() {
+  float get_nonzero_ratio()
+  {
     float non_zero_cnt = 0;
     auto it = um_ij2w.begin();
-    while (it != um_ij2w.end()) {
+    while (it != um_ij2w.end())
+    {
       non_zero_cnt++;
       it++;
     }
 
-    return non_zero_cnt / (float(dim_row) * float(dim_column));
+    return 2 * non_zero_cnt / (float(dim_row) * float(dim_column));
   }
 
   /**
@@ -156,15 +189,18 @@ template <typename DataType> struct SparseMatrixDOK {
    *
    * @param filepath
    */
-  void save(string filepath) {
+  void save(string filepath)
+  {
     ofstream adj_mat_file;
     adj_mat_file.open(filepath, ios::out);
     adj_mat_file << dim_row << "\n";
     int temp_num_link = 0;
-    for (int i = 0; i < dim_row; i++) {
+    for (int i = 0; i < dim_row; i++)
+    {
       temp_num_link = row2nonzero_idx[i].size();
       adj_mat_file << temp_num_link << " ";
-      for (int j = 0; j < temp_num_link; j++) {
+      for (int j = 0; j < temp_num_link; j++)
+      {
         adj_mat_file << row2nonzero_idx[i][j] << " "
                      << (*this)(i, row2nonzero_idx[i][j]) << " ";
       }
