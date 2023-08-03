98c1a16d5347b0cb173eb5ddd461dab72201218b
Koukyosyumei <koukyosyumei@hotmail.com>
2023-08-03 16:12:05 +0900

    update

--- script/download.sh
+++ script/download.sh
@@ -21,3 +21,5 @@ wget -P data/fars https://www.openml.org/data/download/4965247/fars.arff
 
 wget -P data/obesity "https://archive.ics.uci.edu/ml/machine-learning-databases/00544/ObesityDataSet_raw_and_data_sinthetic (2).zip"
 unzip "data/obesity/ObesityDataSet_raw_and_data_sinthetic (2).zip" -d data/obesity
+
+wget -P data/mnist https://www.openml.org/data/download/52667/mnist_784.arff
