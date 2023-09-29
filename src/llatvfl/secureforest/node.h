a25c1074c19499b3ab080dd0a8c4f98415a530da
Koukyosyumei <koukyosyumei@hotmail.com>
2023-09-29 10:56:53 +0900

    update

--- src/llatvfl/secureforest/node.h
+++ src/llatvfl/secureforest/node.h
@@ -429,8 +429,18 @@ struct SecureForestNode : Node<SecureForestParty> {
     if (!(((left->not_splitted_flag && right->not_splitted_flag)) ||
           (left->lmir_flag_exclude_passive_parties &&
            right->lmir_flag_exclude_passive_parties))) {
-      idxs.clear();
-      idxs.shrink_to_fit();
+      // idxs.clear();
+      // idxs.shrink_to_fit();
+    }
+
+    if ((left->is_leaf_flag == 1) && (right->is_leaf_flag == 1) &&
+        (party_id == active_party_id)) {
+      is_all_active_within_subsequent_children = true;
+    }
+    if ((left->is_all_active_within_subsequent_children) &&
+        (right->is_all_active_within_subsequent_children) &&
+        (party_id == active_party_id)) {
+      is_all_active_within_subsequent_children = true;
     }
   }
 
